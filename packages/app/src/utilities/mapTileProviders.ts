7fcbcaaef15519778e996b2470465366db01d77d
Josh <josh@bashed.sh>
2026-03-24 22:12:15 -0700

    feat(app): add map tile provider configuration

---
+++ packages/app/src/utilities/mapTileProviders.ts
@@ -0,0 +1,29 @@
+/**
+ * Map providers available in the application.
+ *
+ * Each provider includes a name, URL template, and attribution information.
+ */
+export interface MapTileProvider {
+    name: string;
+    url: string;
+    attribution: string;
+}
+
+/**
+ * The collection of map providers, including a default provider.
+ */
+export type MapTileProviders = {
+    default: string;
+    providers: Record<string, MapTileProvider>;
+};
+
+export const mapTileProviders = {
+    default: '4565564b-20c7-4303-8c9b-e226399d6cea',
+    providers: {
+        '4565564b-20c7-4303-8c9b-e226399d6cea': {
+            attribution: '© Microsoft Corporation',
+            name: 'Bing Aerial Maps',
+            url: 'https://ecn.t0.tiles.virtualearth.net/tiles/a{quadKey}.jpeg?g=15496&pr=odbl&n=z',
+        },
+    },
+} as const satisfies MapTileProviders;

f24d202e3fd2923a49917a7b1fc00af1048aa0ab
Josh <josh@bashed.sh>
2026-04-20 14:29:56 -0700

    feat(app): add Esri World Imagery tile provider

--- packages/app/src/utilities/mapTileProviders.ts
+++ packages/app/src/utilities/mapTileProviders.ts
@@ -25,5 +25,10 @@ export const mapTileProviders = {
             name: 'Bing Aerial Maps',
             url: 'https://ecn.t0.tiles.virtualearth.net/tiles/a{quadKey}.jpeg?g=15496&pr=odbl&n=z',
         },
+        'ba8ea4a8-8aa8-427c-9426-dfe3c46d38b5': {
+            attribution: '© Esri',
+            name: 'Esri World Imagery',
+            url: 'https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        },
     },
 } as const satisfies MapTileProviders;
